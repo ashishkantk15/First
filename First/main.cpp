116ee2248abe9ee7ed5b25c022ddf99c44de5f9c
ashishkantk15 <ashishkant181@gmail.com>
2024-09-02 15:47:09 +0530

    Initial Commit

---
+++ First/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  First
+//
+//  Created by Ashishkant on 02/09/24.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout<<"enter 2 nos";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum is "<<c;
+    
+    
+    
+    return 0;
+}
